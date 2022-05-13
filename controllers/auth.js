ead8e74269beae8553390bf3b6bf7dc55f87921a
Felipe <felipevaz444@gmail.com>
2022-05-13 15:34:33 -0300

    :sparkles: Seção 19 Concluida

--- controllers/auth.js
+++ controllers/auth.js
@@ -94,7 +94,11 @@ exports.postLogin = (req, res, next) => {
         }
       })
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err)
+      error.httpStatus = 500
+      return next(error)
+    });
 };
 
 exports.postSignup = (req, res, next) => {
@@ -130,6 +134,7 @@ exports.postSignup = (req, res, next) => {
     //   html: '<h1>Deu certo!</h1>'
     // })
   })
+  
 };
 
 exports.postLogout = (req, res, next) => {
