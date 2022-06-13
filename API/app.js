1eda4b09a4713a159a5b85dcc132f0e70ff8d850
Felipe <felipevaz444@gmail.com>
2022-06-13 16:12:45 -0300

    :bug: Mini bug

--- API/app.js
+++ API/app.js
@@ -52,7 +52,7 @@ app.use((err, req, res, next) => {
 mongoose.connect(MONGODB_URI)
   .then(result => {
     const server = app.listen(8080);
-    const io = require('./socket')(server)
+    const io = require('./socket').init(server)
     io.on('connection', socket => {
       console.log('Client Connected')
     })
